48134c30228bbd4e25afda2d1e4d6ba11dc47902
Luana Guillande <luana@31solutions.com>
2022-02-08 17:26:40 -0300

    pos componentes curriculares

--- app/data/pos.js
+++ app/data/pos.js
@@ -58,7 +58,47 @@ let pos = [
                     "Elaboração de matrizes curriculares inovadoras"
                 ]
             },
-
+            
+            {
+                titulo: "Gestão Financeira",
+                itens:[
+                    "Orçamento",
+                    "Gestão de custos e despesas",
+                    "Indicadores de Desempenho",
+                ]
+            },
+            {
+                titulo: "Gestão Administrativa e de Processos",
+                itens:[
+                    "Gestão Ágil em instituições de ensino",
+                    "Processos motivacionais",
+                ]
+            },
+            {
+                titulo: "Gestão da Tecnologia, do EAD e dos Modelos Híbridos",
+                itens:[
+                    "Tecnologia aplicada à gestão e ao ensino",
+                    "O EAD e seus diversos modelos",
+                    "O futuro da tecnologia na educação",
+                ]
+            },
+            {
+                titulo: "Planejamento Estratégico de instituições de ensino",
+                itens:[
+                    "Matriz SWOT",
+                    "Oportunidades e Ameaças no atual cenário educacional",
+                    "Estratégias de diferenciação",
+                ]
+            },
+            {
+                titulo: "Modelos Inovadores em Gestão de Pessoas",
+                itens:[
+                    "Empreendedorismo e gestão de carreira docente na educação superior",
+                    "Cultura e clima organizacional em instituições de ensino",
+                    "Processos motivacionais e de engajamento",
+                ]
+            },
+            
         ]
     },
     {
@@ -78,6 +118,88 @@ let pos = [
                 centavos: 0,
                 label: '12 parcelas de'
             }
+        ],
+        matriz: [
+            {
+                titulo: "Compreendendo como acontece o aprendizado",
+                matriz:[
+                    "Mecanismos neurofisiológicos da aprendizagem",
+                    "Aprendizagem efetiva",
+                    "Aprender a aprender"
+                ]
+            },
+            {
+                titulo: "Modelos Curriculares Inovadores",
+                matriz:[
+                    "Trabalhabilidade e empregabilidade no século XXI",
+                    "Integração horizontal e vertical de currículos",
+                    "Instrumentos Avaliativos Inovadores"
+                ]
+            },
+            {
+                titulo: "Metodologias Ativas de aprendizagem",
+                matriz:[
+                    "Princípios da aprendizagem ativa",
+                    "Tipos de metodologias ativas",
+                    "Aplicabilidade das metodologias ativas"
+                ]
+            },
+            {
+                titulo: "Aprendizagem baseada em desafios e projetos",
+                matriz:[
+                    "Metodologias de projetos",
+                    "Projetos de escopo fechado e de escopo aberto",
+                    "Estruturação de desafios de aprendizagem"
+                ]
+            },
+            {
+                titulo: "Planejamento de atividades de aprendizagem",
+                matriz:[
+                    "Elaboração de atividades de aprendizagem",
+                    "Roteirização de atividades de aprendizagem",
+                    "Atividades baseadas em conteúdo versus atividades baseadas em competências"
+                ]
+            },
+            {
+                titulo: "Educação Híbrida e o uso da Tecnologia",
+                matriz:[
+                    "Modelos híbridos de educação",
+                    "Tecnologias educacionais emergentes",
+                    "Metodologia para o uso adequado da tecnologia"
+                ]
+            },
+            {
+                titulo: "Estratégias de engajamento e acompanhamento do estudante",
+                matriz:[
+                    "Habilidades socioemocionais em contexto educacional",
+                    "Formas de feedback",
+                    "Processos motivacionais"
+                ]
+            },
+            {
+                titulo: "Inteligência Evolutiva",
+                matriz:[
+                    "Habilidade de discernir",
+                    "Habilidade de priorizar",
+                    "Visão sistêmica"
+                ]
+            },
+            {
+                titulo: "Educação e Aprendizagem Evolutiva",
+                matriz:[
+                    "Aprender para evoluir",
+                    "Aprender a evoluir",
+                    "Educar para evoluir"
+                ]
+            },
+            {
+                titulo: "O perfil do professor do século XXI",
+                matriz:[
+                    "Empreendedorismo e gestão de carreira docente na educação superior",
+                    "Professor Preceptor",
+                    "Acompanhamento do processo da aprendizagem"
+                ]
+            },
         ]
     },
     {
@@ -85,7 +207,7 @@ let pos = [
         name: 'Especialização em Psico-oncologia',
         img: '/mbas/mba-3.jpg',
         competencias: [],
-      
+        
         carga_horaria: 360,
         
         descricao: '<p>Occaecat nulla commodo aute dolore enim ipsum enim aute fugiat cupidatat nulla est occaecat dolore. Sint do irure veniam Lorem aliquip cillum ut magna. In nisi minim enim irure et duis dolor qui eiusmod. Consectetur magna sunt nostrud velit. Laboris occaecat Lorem et aliqua dolore.</p>',
