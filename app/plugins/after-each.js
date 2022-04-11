542386d6261b0a337b7abed13a7824bc478eeea1
Luana Guillande <luana@31solutions.com>
2022-04-11 15:22:43 -0300

    teste opacity

--- app/plugins/after-each.js
+++ app/plugins/after-each.js
@@ -1,18 +1,10 @@
 export default async ({ app }) => {
 
   app.router.afterEach((to, from) => {
-    console.log(AOS)
       AOS.init();
       setTimeout(() => {
           AOS.refreshHard();
       }, 2000);
   });
-
-
-  // app.router.beforeEach((to, from) => {
-  //   console.log('befores')
-  //     AOS.init();
-  //     AOS.refresh();
-  // });
 
 }
