dae6ba878e646d9b129d7d672768ae7216ae6a28
Luana Guillande <luana@31solutions.com>
2022-04-20 15:29:56 -0300

    redirect marcas multiversa

---
+++ app/pages/marcas.vue
@@ -0,0 +1,13 @@
+<template></template>
+<script>
+export default {
+  mounted() {
+    open(
+      'https://drive.google.com/drive/folders/1LHRpIJ5YeRCd2CvGfl85UZajkjtGZf1A?usp=sharing',
+      '_self'
+    )
+  },
+}
+</script>
+<style lang='scss'>
+</style>
