79f4636ac7c3cd2497f34d9bec070925b247242e
Luana Guillande <luana@31solutions.com>
2022-04-26 18:08:14 -0300

    teste

--- app/plugins/imagine.js
+++ app/plugins/imagine.js
@@ -5,7 +5,7 @@ Vue.mixin({
             if (image == undefined) return '';
             let barra = image.substring(0, 1) == '/' ? '' : '/';
             let url = process.env.baseUrl === 'http://localhost:3000' ? 'http://localhost:3000' : 'https://ik.imagekit.io/cyrcl27sn/'
-console.log(process, window.origin)
+console.log(process.env._HTTP_BASE_URL_)
             return url + image;
         }
     }
