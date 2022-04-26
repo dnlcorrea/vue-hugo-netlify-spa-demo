e0fe2d8942c6212e2bec885dd16c3912709b194b
Luana Guillande <luana@31solutions.com>
2022-04-26 17:58:23 -0300

    teste2

--- app/plugins/imagine.js
+++ app/plugins/imagine.js
@@ -4,7 +4,7 @@ Vue.mixin({
 	imagine: image => {
             if (image == undefined) return '';
             let barra = image.substring(0, 1) == '/' ? '' : '/';
-            let url = process.env.baseUrl === 'https://multiversa.com/' ? 'https://ik.imagekit.io/cyrcl27sn/' : 'http://localhost:3000'
+            let url = process.env.baseUrl === 'http://localhost:3000' ? 'http://localhost:3000' : 'https://ik.imagekit.io/cyrcl27sn/'
 console.log(process.env.baseUrl)
             return url + image;
         }
